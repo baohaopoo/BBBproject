e85e4aba5daa9bd6294dc9d21bcc5a4884a4b04f
baohaopoo <baohaopoo@gmail.com>
2020-03-12 21:49:12 +0900

    add player,camera controller

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public float offsetX = -400f;
+    public float offsetY = 400f;
+    public float offsetZ = 0f;
+    Vector3 cameraPosition;
+
+    //Zoom 관련
+    public Transform ZoomTarget;
+    public float Zoom;
+    private Transform tr;
+       
+    //회전 관련
+    public float rotateSpeed = 10f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        tr = GetComponent<Transform>();
+
+
+
+
+
+
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ZoomInOut(); 
+    }
+    private void LateUpdate()
+    {
+
+        cameraPosition.x = player.transform.position.x + offsetX;
+        cameraPosition.y = player.transform.position.y + offsetY;
+        cameraPosition.z = player.transform.position.z + offsetZ;
+
+        transform.position = cameraPosition;
+
+        ZoomInOut();
+    }
+    private void ZoomInOut()
+    {
+
+        Vector3 TargetDist = tr.position - ZoomTarget.position;
+        TargetDist = Vector3.Normalize(TargetDist);
+
+        tr.position -= (TargetDist * Input.GetAxis("Mouse ScrollWheel") * Zoom);
+    
+    
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 50f;
+    public float jumpPower = 5f;
+
+    Rigidbody rigdbody;
+    Collider col;
+
+    Vector3 movement;
+    float horizontalMove;
+    float verticalMove;
+    bool isJumping;
+
+    void Awake()
+    {
+        rigdbody = GetComponent<Rigidbody>();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+
+
+        if (Input.GetButtonDown("Jump"))
+            isJumping = true;
+        
+    }
+
+    void FixedUpdate()
+    {
+      //물리만 다루는 곳.
+        Run();
+        Jump();
+        Turn();
+    }
+
+    void Run()
+    {
+        movement.Set(horizontalMove, 0, verticalMove);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rigdbody.MovePosition(transform.position + movement);
+
+    
+    }
+
+    void Jump()
+    {
+        if (!isJumping)
+            return;
+
+        rigdbody.MovePosition(transform.position + Vector3.up); //단순높이
+        rigdbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+
+        isJumping = false;
+    
+    
+    }
+
+    void Turn()
+    {
+
+        if (horizontalMove == 0 && verticalMove == 0)
+            return;
+
+        Quaternion playerRotation = Quaternion.LookRotation(movement);
+        rigdbody.rotation = Quaternion.Slerp(rigdbody.rotation, playerRotation, Time.deltaTime);
+  
+    }
+}
