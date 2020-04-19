d0ee8c37b6dd21577fd3bbdfb181fceaa19ca22d
Lee Min Hee <yeminhae@kpu.ac.kr>
2020-04-19 14:18:09 +0900

    Revert "apply animation"
    This reverts commit 38e2b82602e663f2733665a9e41a54369d6af209.

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -12,13 +12,13 @@ public class CameraController : MonoBehaviour
     public float offsetZ = 0f;
     Vector3 cameraPosition;
 
-    //zoom 관련
-    public float zoomSpeed = 10f;
-
+    //Zoom 관련
+    public Transform ZoomTarget;
+    public float Zoom;
+    private Transform tr;
+       
     //회전 관련
     public float rotateSpeed = 10f;
-
-    private Camera mainCamera;
 
 
 
@@ -26,20 +26,19 @@ public class CameraController : MonoBehaviour
     void Start()
     {
 
-        mainCamera = GetComponent<Camera>();
+        tr = GetComponent<Transform>();
 
 
 
 
 
 
-    }
+}
 
     // Update is called once per frame
     void Update()
     {
-        ZoomInOut();
-        Rotate();
+        //ZoomInOut(); 
     }
     private void LateUpdate()
     {
@@ -55,30 +54,11 @@ public class CameraController : MonoBehaviour
     private void ZoomInOut()
     {
 
-        float distance = Input.GetAxis("Mouse ScrollWheel") * -1 * zoomSpeed;
-        if (distance != 0)
-        {
-            mainCamera.fieldOfView += distance;
-        }
-    }
-    void Rotate()
-    {
+        Vector3 TargetDist = tr.position - ZoomTarget.position;
+        TargetDist = Vector3.Normalize(TargetDist);
 
-        if (Input.GetMouseButton(1))
-        {
-
-            Vector3 rot = transform.rotation.eulerAngles;
-            rot.y += Input.GetAxis("Mouse X") * rotateSpeed;
-            rot.x += -1 * Input.GetAxis("Mouse Y") * rotateSpeed;
-            Quaternion q = Quaternion.Euler(rot);
-            q.z = 0;
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, q, 2f);
-
-
-        }
-
-
-
+        tr.position -= (TargetDist * Input.GetAxis("Mouse ScrollWheel") * Zoom);
+    
+    
     }
 }
