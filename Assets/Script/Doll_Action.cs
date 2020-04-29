f92b121108386514b12e3b117d246404dddda1fe
Hong Seung Hye <ghdtmdgp@icloud.com>
2020-04-29 21:12:33 +0900

    0429

---
+++ Assets/Script/Doll_Action.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Doll_Action : MonoBehaviour
+{
+
+    GameObject player;
+    GameObject playerGrabPoint;
+    GameObject UIImage;
+    Vector3 movement;
+    bool isPlayerEnter;
+
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerGrabPoint = GameObject.FindGameObjectWithTag("GrabPoint");
+        UIImage = GameObject.FindGameObjectWithTag("UIImage");
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("p") && isPlayerEnter) {
+            transform.SetParent(playerGrabPoint.transform);
+            transform.localPosition = Vector3.zero;
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+
+            isPlayerEnter = false;
+
+        
+        }
+        
+    }
+    public void Pickup(GameObject item)
+    {
+        SetGrab(item, true);
+        
+        //애니메이션 줍는 모션을 넣으면 됨.
+       // Animator.SetTrigger("")
+
+        //isPicking = truel
+    
+    
+    }
+
+
+    void Drop()
+    {
+        GameObject item = playerGrabPoint.GetComponentInChildren<Rigidbody>().gameObject;
+        SetGrab(item, false);
+
+        playerGrabPoint.transform.DetachChildren();
+    
+    
+    }
+
+    void SetGrab(GameObject item, bool isGrab)
+    {
+        Collider[] itemColliders = item.GetComponents<Collider>();
+        Rigidbody itemRigidbody = item.GetComponent<Rigidbody>();
+
+        foreach (Collider itemCollider in itemColliders) {
+            itemCollider.enabled = !isGrab;
+
+        }
+        itemRigidbody.isKinematic = isGrab;
+
+    
+    
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player) {
+            isPlayerEnter = true;
+
+            //애니메이션을 위로 올라가는 애니메이션을 쓴다.
+
+
+            //위로 올라가며 위에 자리를 잡는다.
+
+
+            //movement.Set(0, 9, 0);
+            //transform.position += movement;
+
+
+            //친구 구하면 UI.
+            if (Input.GetMouseButtonDown(0))
+            {
+                
+                UIImage.gameObject.SetActive(true);
+                Debug.Log("5번");
+                Destroy(transform.gameObject);
+                Debug.Log("친구 하나 구했다.");
+
+
+            }
+
+
+
+            //친구 하나 구할때마다 1씩 증가
+            //Score_Manager.savedollNum += 1;
+
+
+
+
+        }
+        
+
+
+
+ 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player) {
+            isPlayerEnter = false;
+
+        }
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,68 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;// 앞뒤 움직임 걷기속도 
-    public float rotateSpeed = 180f; // 좌우 회전 속도
-    public float jumpPower = 0f;
+    public float speed = 50f;
+    public float jumpPower = 5f;
 
-    private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
-    private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
-    private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
+    Rigidbody rigdbody;
+    Collider col;
 
-    bool isJumping=false;
+    Vector3 movement;
+    float horizontalMove;
+    float verticalMove;
+    bool isJumping;
 
-    //int jumpcount = 0;
+    void Awake()
+    {
+        rigdbody = GetComponent<Rigidbody>();
 
+    }
     // Start is called before the first frame update
     void Start()
     {
-        playerInput = GetComponent<PlayerInput>();
-        playerRigidbody = GetComponent<Rigidbody>();
-        playerAnimator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
+
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+
+
         if (Input.GetButtonDown("Jump"))
-        {
             isJumping = true;
-        }
-
-
-        playerAnimator.SetFloat("Move", playerInput.move);
-        playerAnimator.SetBool("Jump", isJumping);
-
+        
     }
 
     void FixedUpdate()
     {
-      //물리만 다루는 곳.;
+      //물리만 다루는 곳.
+        Run();
         Jump();
-        Rotate();
-        Move();
-
-
-
+        Turn();
     }
 
-    // 입력값에 따라 캐릭터를 앞뒤로 움직임
-    private void Move()
+    void Run()
     {
-        Vector3 moveDistance =
-            playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
+        movement.Set(horizontalMove, 0, verticalMove);
+        movement = movement.normalized * speed * Time.deltaTime;
 
-        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
-    }
+        rigdbody.MovePosition(transform.position + movement);
 
-    // 입력값에 따라 캐릭터를 좌우로 회전
-    private void Rotate()
-    {
-        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
-        playerRigidbody.rotation =
-            playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
-
+    
     }
 
     void Jump()
@@ -73,13 +63,22 @@ public class PlayerController : MonoBehaviour
         if (!isJumping)
             return;
 
-        playerRigidbody.MovePosition(transform.position + Vector3.up); //단순높이
-        playerRigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        rigdbody.MovePosition(transform.position + Vector3.up); //단순높이
+        rigdbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
 
         isJumping = false;
     
     
     }
 
+    void Turn()
+    {
 
+        if (horizontalMove == 0 && verticalMove == 0)
+            return;
+         
+        Quaternion playerRotation = Quaternion.LookRotation(movement);
+        rigdbody.rotation = Quaternion.Slerp(rigdbody.rotation, playerRotation, Time.deltaTime);
+  
+    }
 }
