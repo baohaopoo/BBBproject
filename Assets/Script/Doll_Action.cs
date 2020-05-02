87cdc833902525ea0e4f1d5648827b34374b35e6
Lee Min Hee <yeminhae@kpu.ac.kr>
2020-05-03 06:02:03 +0900

    test

--- Assets/Script/Doll_Action.cs
+++ Assets/Script/Doll_Action.cs
@@ -14,14 +14,13 @@ public class Doll_Action : MonoBehaviour
     Vector3 movement;
     bool isPlayerEnter;
 
-    GameObject bo;
+
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        playerGrabPoint = GameObject.FindGameObjectWithTag("grabPoint");
         UIImage = GameObject.FindGameObjectWithTag("UIImage");
         audioSource = GetComponent<AudioSource>();
-        
+
     }
     // Start is called before the first frame update
     void Start()
@@ -32,42 +31,53 @@ public class Doll_Action : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // audioSource.Play();
-        pickup();
+        audioSource.Play();
+
         
+    }
+    public void Pickup(GameObject item)
+    {
+        SetGrab(item, true);
+        
+        //애니메이션 줍는 모션을 넣으면 됨.
+       // Animator.SetTrigger("")
+
+        //isPicking = truel
+    
+    
     }
 
 
-
-    void pickup()
+    void Drop()
     {
+        GameObject item = playerGrabPoint.GetComponentInChildren<Rigidbody>().gameObject;
+        SetGrab(item, false);
 
-        if (isPlayerEnter)
-        {
-            if (Input.GetMouseButton(0))
-            {
+        playerGrabPoint.transform.DetachChildren();
+    
+    
+    }
 
+    void SetGrab(GameObject item, bool isGrab)
+    {
+        Collider[] itemColliders = item.GetComponents<Collider>();
+        Rigidbody itemRigidbody = item.GetComponent<Rigidbody>();
 
-                //부모를 grabPoint
-                transform.SetParent(playerGrabPoint.transform);
-                transform.localPosition = Vector3.zero;
-                transform.rotation = new Quaternion(0, 0, 0, 0);
-
-
-
-
-
-            }
-
+        foreach (Collider itemCollider in itemColliders) {
+            itemCollider.enabled = !isGrab;
 
         }
+        itemRigidbody.isKinematic = isGrab;
 
-
+    
+    
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player) {
             isPlayerEnter = true;
+
+   
 
         }
         
