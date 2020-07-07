b3aaa52406da2d4bfbfa43e49267ade3393b3d6e
Lee Min Hee <baohaopoo@gmail.com>
2020-07-07 17:13:01 +0900

    server1.0

--- Assets/01.Script/LobbyManager.cs
+++ Assets/01.Script/LobbyManager.cs
@@ -10,15 +10,20 @@ using UnityEngine.UI;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
-    private string gameVersion = "1"; //게임 버전
+    private string gameVersion = "Kidsroom 1.0"; //게임 버전
+
     public Text connectionInfoText; //네트워크 정보를 표시할 텍스트
     public Button joinButton; //룸접속버튼
 
     //게임 실행과 동시에 마스터 서버 접속 시도
     void Start()
     {
-        PhotonNetwork.GameVersion = gameVersion;
-        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = gameVersion; //접속에 필요한 정보설정
+        PhotonNetwork.ConnectUsingSettings(); //설정한 정보로 마스터 서버 접속 시도
+
+
+        Debug.Log("마스터 서버에 접속중..");
+
 
         joinButton.interactable = false; //접속하는 동안에 룸 접속 못하도록 접속 버튼 비활.
         connectionInfoText.text = "마스터 서버에 접속중..";
@@ -29,6 +34,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster() {
         joinButton.interactable = true;
         connectionInfoText.text = "온라인 : 마스터 서버와 연결됨";
+        Debug.Log("온라인 : 마스터 서버와 연결됨");
 
     }
 
@@ -37,7 +43,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     {
         joinButton.interactable = false;
         connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음 \n접속 재시도 중...";
-
+        Debug.Log("오프라인: 마스터 서버와 연결되지 않음 ,접속 재시도 중.");
         PhotonNetwork.ConnectUsingSettings();
     }
 
@@ -51,6 +57,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
             //룸 접속 실행
             connectionInfoText.text = "룸에 접속...";
             PhotonNetwork.JoinRandomRoom();
+            Debug.Log("룸에 접속..");
         }
         else
         {
@@ -62,7 +69,10 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     }
 
     //빈방이 없어 랜덤 룸 참가에 실패한 경우 자동 실행
-    public override void OnJoinRandomFailed(short returnCode, string message) { 
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        Debug.Log("No Room");
+        connectionInfoText.text = "빈 방이 없음.. 새로운 방 생성..";
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
 
     }
 
@@ -70,6 +80,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         connectionInfoText.text = "방 참가 성공";
+        Debug.Log("Joined room");
         PhotonNetwork.LoadLevel("Kidsroom"); //모든 룸 참가자가 Kidsroom씬을 로드하게 함.
         
     }
