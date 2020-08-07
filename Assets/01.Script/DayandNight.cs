ec67b7998d0c6a6cce3e9098e5ad1aff1a24143c
Hong Seung Hye <ghdtmdgp@icloud.com>
2020-08-08 00:50:19 +0900

    시간에따라 창문 색 변함

--- Assets/01.Script/DayandNight.cs
+++ Assets/01.Script/DayandNight.cs
@@ -7,10 +7,11 @@ public class DayandNight : MonoBehaviour
     [SerializeField]
     private float gameTime=5; 
     private Transform SunTransform;
-    private float rotX;
     private Sky skyimg;
     public float DayTime; //몇초에 한번씩 낮밤 바뀔지 (현실시간)
-    private float lastTime; 
+    private float lastTime;
+
+
     private void Start()
     {
         SunTransform = GetComponent<Transform>();
@@ -20,21 +21,17 @@ public class DayandNight : MonoBehaviour
     }
     void Update()
     {
-        transform.Rotate(Vector3.right, 0.1f * gameTime * Time.deltaTime);
-
-        rotX = SunTransform.rotation.x;
-
-
+        transform.Rotate(Vector3.right, 0.1f * gameTime * Time.deltaTime); 
+ 
         // Debug.Log("rotX:"+rotX+",Time:"+(Time.time-lastTime));
         if (Time.time > lastTime + DayTime)
         {
             lastTime = Time.time;
-            skyimg.inNight = !skyimg.inNight; //밤낮 바꾸기
             skyimg.progressDawn = 0;
             skyimg.progressDay = 0;
             skyimg.progressEvening = 0;
             skyimg.progressNight = 0;
-
+            skyimg.inNight = !skyimg.inNight;
 
         }
 
